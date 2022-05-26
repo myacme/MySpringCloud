3e92e983130389a0c06a10586941b6bdf70b234e
liujixiang <1030375081@qq.com>
2022-05-26 15:06:05 +0800

    提交

--- alibaba-provide-server-9001/src/main/java/com/example/demo/controller/Controller.java
+++ alibaba-provide-server-9001/src/main/java/com/example/demo/controller/Controller.java
@@ -1,10 +1,10 @@
 package com.example.demo.controller;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -32,5 +32,20 @@ public class Controller {
 	@GetMapping()
 	public String production() {
 		return "我是Nacos生产者：" + port;
+	}
+
+
+	@GetMapping("/hotKey")
+	@SentinelResource(value = "hotKey", blockHandler = "hotKeyBlock", fallback = "fallBack")
+	public String hotKey() {
+		return "sentinel 热键限流测试 ";
+	}
+
+	public String hotKeyBlock() {
+		return "限流规则异常方法";
+	}
+
+	public String fallBack() {
+		return "Java异常方法";
 	}
 }
